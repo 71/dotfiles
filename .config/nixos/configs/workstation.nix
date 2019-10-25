702f00754e80e855f40e241b57e96f5b29a6f043
Grégoire Geis <git@gregoirege.is>
2019-10-25 02:31:40 +0200

    Allow Chromecast and hardware 2FA.

--- .config/nixos/configs/workstation.nix
+++ .config/nixos/configs/workstation.nix
@@ -10,4 +10,12 @@
 
   networking.hostName = "Penrose";
   networking.wireless.enable = true;
+
+  # Enable hardware 2FA
+  hardware.u2f.enable = true;
+
+  # Enable Chromecast
+  networking.firewall.extraCommands = ''
+    iptables -A nixos-fw -p udp -m udp -s 192.168.0.0/16 --match multiport --dports 1900,5353 -j nixos-fw-accept
+  '';
 }
